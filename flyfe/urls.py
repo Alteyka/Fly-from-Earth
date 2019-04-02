f49cb5e696e3902bbf859a4b50b885af5bb2ae0f
Alteyka <alteyka.da@gmail.com>
2019-04-02 11:45:34 +0300

    Created some templates and added machanism generate random cards.

--- flyfe/urls.py
+++ flyfe/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', cards_list, name='cards_list_url'),
-    path('card/<str:slug>/', CardDetail.as_view(), name='card_detail_url')
+    path('cards_list/', cards_list, name='cards_list_url'),
+    path('card/<str:slug>/', CardDetail.as_view(), name='card_detail_url'),
+    path('', start_page, name='start_page_url'),
+    path('random_card/', random_card, name='random_card_url')
 ]
--- flyfe/views.py
+++ flyfe/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
+import random
+
 from .models import *
 
 
@@ -12,5 +14,13 @@ def cards_list(request):
 
 class CardDetail(View):
     def get(self, request, slug):
-        card = get_object_404(Card, slug__iexact=slug)
+        card = get_object_or_404(Card, slug__iexact=slug)
         return render(request, 'flyfe/card_detail.html', context={'card': card})
+
+def start_page(request):
+    return render(request, 'flyfe/start_page.html')
+
+def random_card(request):
+    cards = list(Card.objects.all())
+    card = random.choice(cards)
+    return render(request, 'flyfe/card_detail.html', context={'card': card})
