0b5fd184e6deebcfbe68f92823ea596380f4f7da
Alteyka <alteyka.da@gmail.com>
2019-05-06 11:35:06 +0300

    Some changes in templates

--- flyfe/forms.py
+++ flyfe/forms.py
@@ -26,8 +26,8 @@ class NewPlanetForm(forms.ModelForm):
         return new_slug
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
 
 
 class RegistrationForm(UserCreationForm):
