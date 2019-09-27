e10ed5535589d647121707f9edce6f11be82750d
Arthur <alteyka.da@gmail.com>
2019-09-27 16:20:08 +0300

    First tests complete successful

--- flyfe/tests.py
+++ flyfe/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from selenium import webdriver
+import time
+
+try:
+    link = 'http://127.0.0.1:8000/flyfe/'
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    find_button = browser.find_element_by_id('main-button-start_page')
+    find_button.click()
+
+    time.sleep(5)
+
+    text = browser.find_element_by_id('login-text')
+    login_text = text.text
+
+    assert "Login" == login_text
+
+finally:
+    time.sleep(5)
+    browser.quit()
+
